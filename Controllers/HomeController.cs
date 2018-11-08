89b54f0777bcc8497a39dd29f1e27bc2d7229973
Seano <seanoliverrobinson@gmail.com>
2018-11-07 22:06:34 -0500

    First Upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CoffeeShopApp.Models;
+
+namespace CoffeeShopApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult RegistrationPage()
+        {
+            return View();
+        }
+
+        public ActionResult WelcomeUser(AddMember newUser)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.ConfirmMessage = "Welcome! " + newUser.FirstName;
+                return View("Confirm");  //GO HERE DANG IT!!!
+            }
+            else
+            {
+                return View("Error"); //So it goes to the Error page fine but it won't go to the confirm page.
+            }
+        }
+    }
+}
---
+++ Models/AddMember.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeShopApp.Models
+{
+    public class AddMember
+    {
+        //Commented out a bunch of code here to see if the test page would work. It's Not....WHHHYYYYYYYYYY C# God Help me!
+        [Required]
+        [RegularExpression("^[a-zA-Z]{2,}$")]
+        public string FirstName { set; get; }
+
+        [Required]
+        [RegularExpression("^[a-zA-Z]{2,}$")]
+        public string LastName { set; get; }
+
+        /*[Required]
+        [RegularExpression("/^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g")] //I got this code from Regex, I am not sure why it isn't work.
+        public string Email { set; get; }  */
+
+        [Required]
+        [RegularExpression("^[0 - 9]{3}-[0-9]{3}-[0-9]{4}$")]  
+        public string PhoneNumber { set; get; }  //Ask later why turning this into an Int it won't work in the constructor See below
+
+        /*[Required]
+        [RegularExpression("^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$")] //Same with this one and the email one. Need to find out why they aren't working
+        public string Password { set; get; }*/
+
+        public AddMember()
+        {
+            FirstName = " ";
+            LastName = " ";
+            //Email = " ";
+            PhoneNumber = " " ; //How to print the Int on here.
+            //Password = " ";
+        }
+
+
+    }
+}
